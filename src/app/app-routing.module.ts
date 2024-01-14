01a267475c17bf181db895b8d5ab49fc2dd719db
tudormuresan07 <tudorvladmuresan@gmail.com>
2024-01-14 20:38:28 +0200

    AWD Project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { PersonalInfoComponent } from './personal-info/personal-info.component';
+import { ContactComponent } from './contact/contact.component';
+import { SubjectComponent } from './subject/subject.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'main', component: MainComponent },
+  { path: 'personal-info', component: PersonalInfoComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'subject', component: SubjectComponent },
+  { path: '', redirectTo: '/main', pathMatch: 'full' }, // Default route
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PersonalInfoComponent } from './personal-info/personal-info.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MainComponent } from './main/main.component';
+import { ContactComponent } from './contact/contact.component';
+import { SubjectComponent } from './subject/subject.component';
+import { CardModule } from '@smart-webcomponents-angular/card';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonalInfoComponent,
+    NavbarComponent,
+    MainComponent,
+    ContactComponent,
+    SubjectComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
